e24b974ddf051cb2518ea701c34c98ac2417ee5a
SahilAnower <sahilanower2@gmail.com>
2025-05-11 16:37:28 +0530

    Init commit

---
+++ src/main/java/com/aivideo/reelforge/ReelforgeApplication.java
@@ -0,0 +1,13 @@
+package com.aivideo.reelforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReelforgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReelforgeApplication.class, args);
+	}
+
+}
